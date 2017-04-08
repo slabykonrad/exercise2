528741d211169d77e47b20c49cc013679098b2da
slabykonrad <konrad.slaby@wp.pl>
2017-04-08 23:23:19 +0200

    changed implementation of class BufferManualImpl

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -2,6 +2,10 @@ package wdsr.exercise2.procon;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
@@ -9,12 +13,42 @@ import java.util.Queue;
  */
 public class BufferManualImpl implements Buffer {
 	private Queue<Order> queue = new LinkedList<Order>();
-	
-	public synchronized void submitOrder(Order order) throws InterruptedException {
-		queue.offer(order);
+	private final Lock lock = new ReentrantLock();
+	private final Condition bufferNotFull = lock.newCondition();
+	private final Condition bufferNotEmpty = lock.newCondition();
+	private final int MAXCAPACITY = 100000;
+
+	public void submitOrder(Order order) throws InterruptedException {
+		lock.lock();
+
+		try{
+			while(queue.size() == MAXCAPACITY){
+				bufferNotFull.await();
+			}
+
+			if(order != null){
+				queue.offer(order);
+				bufferNotEmpty.signalAll();
+			}
+		}
+		finally{
+			lock.unlock();
+		}
 	}
 	
-	public synchronized Order consumeNextOrder() throws InterruptedException {
-		return queue.remove();
+	public Order consumeNextOrder() throws InterruptedException {
+		lock.lock();
+
+		try{
+			while(queue.isEmpty()){
+				bufferNotEmpty.await();
+			}
+
+			bufferNotFull.signal();
+			return queue.remove();
+		}
+		finally{
+			lock.unlock();
+		}
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -7,7 +7,7 @@ import java.util.concurrent.BlockingQueue;
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
-	private BlockingQueue<Order> blockingQueue = new ArrayBlockingQueue<>(100);
+	private BlockingQueue<Order> blockingQueue = new ArrayBlockingQueue<>(10);
 	
 	public void submitOrder(Order order) throws InterruptedException {
 		blockingQueue.put(order);
